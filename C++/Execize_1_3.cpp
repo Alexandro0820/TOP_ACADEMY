b6551d493f610a1d5a2d14d6718b99fe21e5b0db
Alexandro <lego08kiborg@gmail.com>
2024-06-20 19:47:14 +0300

    first commit

---
+++ C++/Execize_1_3.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+#define X 128
+
+using namespace std;
+
+void EXECIZE_1(char*str, int pos, char*str_2) {
+	int k = 0;
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] != str[pos]) {
+			str_2[k++] = str[i];
+		}
+	}
+	cout << "Ваша новая строка: ";
+	for (int i = 0; i < k; i++) cout << str_2[i];
+}
+
+void EXECIZE_2(char* str, char sym, char* str_2) {
+	int k = 0;
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] != sym) {
+			str_2[k++] = str[i];
+		}
+	}
+	cout << "Ваша новая строка: ";
+	for (int i = 0; i < k; i++) cout << str_2[i];
+}
+
+void EXECIZE_3(char* str, char sym, int pos) {
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == str[pos]) {
+			str[i] = sym;
+		}
+	}
+	cout << "Ваша новая строка: ";
+	for (int i = 0; i < strlen(str); i++) cout << str[i];
+}
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	// Задание 1
+	cout << "Задание 1\n\n";
+	char str[X];
+	int pos;
+	char*str_2 = new char[X - 1];
+	cout << "Введите вашу строку: "; gets_s(str);
+	cout << "Введите номер символа в строке (нумерация начинается с 0): "; cin >> pos;
+	EXECIZE_1(str, pos, str_2);
+	delete[] str_2;
+	cout << "\n\n";
+	// Задание 2
+	cout << "Задание 2\n\n";
+	char STR[X]; char* STR_2 = new char[X - 1];
+	char sym;
+	cout << "Введите вашу строку: "; gets_s(STR); gets_s(STR);
+	cout << "Введите символ: "; cin >> sym;
+	EXECIZE_2(STR, sym, STR_2);
+	delete[] STR_2;
+	cout << "\n\n";
+	// Задание 3
+	cout << "Задание 3\n\n";
+	char strk[X];
+	int POS; char SYM;
+	cout << "Введите вашу строку: "; gets_s(strk); gets_s(strk);
+	cout << "Введите позицию: "; cin >> POS;
+	cout << "Введите символ: "; cin >> SYM;
+	EXECIZE_3(strk, SYM, POS);
+	cout << "\n\n";
+	return 0;
+}
---
+++ C++/Exesize_4_6.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+#define X 128
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	//Задание 4
+	cout << "Задание 4\n\n";
+	char str[X], s = '.', c = '!';
+	cout << "Введите вашу строку: "; gets_s(str);
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == s) str[i] = c;
+		cout << str[i];
+	}
+	cout << "\n\n";
+	//Задание 5
+	cout << "Задание 5\n\n";
+	char STR[X], sym;
+	int count = 0;
+	cout << "Введите вашу строку: "; gets_s(STR);
+	cout << "Введите искомый символ: "; cin >> sym;
+	for (int i = 0; i < strlen(STR); i++) {
+		if (STR[i] == sym) count++;
+	}
+	cout << "Ваш символ встречается в строке " << count << " раз(-а)!";
+	cout << "\n\n";
+	//Задание 6
+	cout << "Задание 6\n\n";
+	char strin[X];
+	int k = 0, u = 0, z = 0;
+	int i = 0;
+	cout << "Введите вашу строку: "; gets_s(strin); gets_s(strin);
+	while (strin[i]){
+		if (strin[i] >= '0' && strin[i] <= '9') k++;
+		else if ((strin[i] >= 'a' && strin[i] <= 'z') || (strin[i] >= 'A' && strin[i] <= 'Z')) u++;
+		else z++;
+		i++;
+	}
+	cout << "Числа: " << k << endl;
+	cout << "Буквы: " << u << endl;
+	cout << "Прочее: " << z << endl;
+	cout << "\n\n";
+	return 0;
+}
